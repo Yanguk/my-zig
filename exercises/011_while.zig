44015696f334ccb2beb02ce0c8b3d4c7865b1e31
Yanguk <yanguk@duck.com>
2025-09-30 20:55:14 +0900

    ~34
    Signed-off-by: Yanguk <yanguk@duck.com>

--- exercises/011_while.zig
+++ exercises/011_while.zig
@@ -21,7 +21,8 @@ pub fn main() void {
     var n: u32 = 2;
 
     // Please use a condition that is true UNTIL "n" reaches 1024:
-    while (???) {
+    const a = comptime 1024;
+    while (n < a) {
         // Print the current number
         std.debug.print("{} ", .{n});
 
